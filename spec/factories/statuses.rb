bf45396e1811ac0585ed0a183efb735af261f915
carlospazuzu <carlosh555@hotmail.com>
2023-02-03 12:20:57 -0300

    test: models associations tests and factories

--- spec/factories/statuses.rb
+++ spec/factories/statuses.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :status do
-    body { "MyText" }
+    body { "Whatever status you would want" }
+
+    transient do
+      media {[]}
+    end
+
+    after(:create) do |status, data|
+      status.media << data.media
+    end
   end
 end
--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -7,4 +7,24 @@ RSpec.describe Status, type: :model do
       it { is_expected.to validate_length_of(:body).is_at_most(300) }
     end
   end
+
+  describe 'associations' do
+    context 'replies' do
+      it { is_expected.to have_many(:replies) }
+    end
+
+    context 'user' do
+      it { is_expected.to belong_to(:user) }
+    end
+
+    context 'media' do
+      it 'is expected to not allow more than 4 medium objects to be attached' do
+        status = Status.new(body: 'whatever')
+        5.times do
+          status.media << Medium.new(kind: 1, url: 'https://google.com/img.png')
+        end
+        is_expected.not_to be_valid
+      end
+    end
+  end
 end
